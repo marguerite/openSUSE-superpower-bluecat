22074c01ca6275bd4bfd4c4c77fbbdc577ea3614
ll0xff <ll0xff0@gmail.com>
2014-08-16 00:04:55 +0800

    add a tip '重建字体缓存'

--- openSUSE-tips.js
+++ openSUSE-tips.js
@@ -1,5 +1,5 @@
-tips = new Array(65);
-code = new Array(65);
+tips = new Array(66);
+code = new Array(66);
 tips[0] = '安装软件包';
 code[0] = 'sudo zypper in <软件包名>';
 tips[1] = '升级软件包';
@@ -132,6 +132,8 @@ tips[64] = '查找并显示指定文本文件中不包含分号的行';
 code[64] = 'grep -v ";" <指定文本文件>';
 tips[65] = 'zypper 安装一个软件包的同时安装/卸载另一个软件包";
 code[65] = 'sudo zypper in <软件包1> +/-<软件包2>";
+tips[66] = '安装新字体后,重建字体缓存'
+code[66] = 'fc-cache -fv'
 
 index = Math.floor(Math.random() * tips.length);
 document.write("&nbsp;"+tips[index]+':&nbsp;&nbsp;<code>'+code[index]+'</code>');
